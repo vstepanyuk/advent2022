44681195a6ce8167b8862e3d91009c10d1079386
Vitali Stsepaniuk <contact@vitaliy.dev>
2022-12-12 07:14:40 +0100

    refactor: day12, optimize second solution

--- src/day12.rs
+++ src/day12.rs
@@ -14,27 +14,32 @@ pub struct DaySolution {
 }
 
 impl DaySolution {
-    fn find_path(
+    fn find_path<SF, WF>(
         &self,
         matrix: &mut Matrix<char>,
-        start: (usize, usize),
-        end: (usize, usize),
-    ) -> Result<usize> {
-        *matrix.get_mut(start.0, start.1).unwrap() = 'a';
-        *matrix.get_mut(end.0, end.1).unwrap() = 'z';
+        start: (char, usize, usize),
+        end: (char, usize, usize),
+        distance: WF,
+        success: SF,
+    ) -> Result<usize>
+    where
+        SF: Fn(&(char, usize, usize)) -> bool,
+        WF: Fn(&char, &char) -> i32,
+    {
+        *matrix.get_mut(start.1, start.2).unwrap() = start.0;
+        *matrix.get_mut(end.1, end.2).unwrap() = end.0;
 
         let result = bfs(
             &start,
-            |&(x, y)| {
-                let current = *matrix.get(x, y).unwrap();
+            |&(current, x, y)| {
                 matrix
                     .neighbours4_iter(x, y)
                     .filter_map(|(&c, (x, y))| {
-                        (c as i32 - current as i32 <= 1).then_some((x as usize, y as usize))
+                        (distance(&c, &current) <= 1).then_some((c, x as usize, y as usize))
                     })
                     .collect::<Vec<_>>()
             },
-            |p| p == &end,
+            success,
         );
 
         Ok(result
@@ -50,33 +55,44 @@ impl Solution for DaySolution {
 
         let start = matrix
             .iter()
-            .find_map(|(c, (x, y))| (*c == 'S').then_some((x, y)))
+            .find_map(|(c, (x, y))| (*c == 'S').then_some(('a', x, y)))
             .ok_or_else(|| anyhow::anyhow!("No start"))?;
 
         let end = matrix
             .iter()
-            .find_map(|(c, (x, y))| (*c == 'E').then_some((x, y)))
+            .find_map(|(c, (x, y))| (*c == 'E').then_some(('z', x, y)))
             .ok_or_else(|| anyhow::anyhow!("No end"))?;
 
-        Ok(Box::new(self.find_path(&mut matrix, start, end)?))
+        Ok(Box::new(self.find_path(
+            &mut matrix,
+            start,
+            end,
+            |n, c| *n as i32 - *c as i32,
+            |p| p.1 == end.1 && p.2 == end.2,
+        )?))
     }
 
     fn part2(&self, input: &str) -> Result<Box<dyn Display>> {
-        let matrix = Matrix::<char>::from(input).ok_or_else(|| anyhow::anyhow!("Invalid input"))?;
+        let mut matrix =
+            Matrix::<char>::from(input).ok_or_else(|| anyhow::anyhow!("Invalid input"))?;
+
+        let start = matrix
+            .iter()
+            .find_map(|(c, (x, y))| (*c == 'S').then_some(('a', x, y)))
+            .ok_or_else(|| anyhow::anyhow!("No start"))?;
 
         let end = matrix
             .iter()
-            .find_map(|(c, (x, y))| (*c == 'E').then_some((x, y)))
+            .find_map(|(c, (x, y))| (*c == 'E').then_some(('z', x, y)))
             .ok_or_else(|| anyhow::anyhow!("No end"))?;
 
-        let result = matrix
-            .iter()
-            .filter_map(|(c, (x, y))| (*c == 'S' || *c == 'a').then_some((x, y)))
-            .filter_map(|start| self.find_path(&mut matrix.clone(), start, end).ok())
-            .min()
-            .unwrap();
-
-        Ok(Box::new(result))
+        Ok(Box::new(self.find_path(
+            &mut matrix,
+            end,
+            start,
+            |n, c| *c as i32 - *n as i32,
+            |&(c, _, _)| c == 'a',
+        )?))
     }
 }
 
