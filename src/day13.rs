6eae0f8cadf0217d82d5340e9359e723f58867ca
Vitali Stsepaniuk <vitali.stsepaniuk@seven.one>
2022-12-13 13:23:06 +0100

    feat(day13): implementation

--- src/day13.rs
+++ src/day13.rs
@@ -1,24 +1,113 @@
-use std::fmt::Display;
+use std::{cmp::Ordering, fmt::Display};
 
 use anyhow::Result;
 use aoc::{Runnable, Solution};
 use aoc_derive::Runner;
+use itertools::Itertools;
 
 #[derive(Runner)]
-#[aoc(file = "inputs/day08.txt")]
+#[aoc(file = "inputs/day13.txt")]
 pub struct DaySolution {
     pub filename: &'static str,
 }
 
-impl DaySolution {}
+type JsonValue = serde_json::Value;
 
-impl Solution for DaySolution {
-    fn part1(&self, _input: &str) -> Result<Box<dyn Display>> {
-        Ok(Box::new(0))
+impl DaySolution {
+    fn parse(&self, input: &str) -> Result<Vec<JsonValue>> {
+        let mut results = vec![];
+
+        for line in input.lines() {
+            if line.trim().is_empty() {
+                continue;
+            }
+
+            results.push(line.parse::<JsonValue>()?);
+        }
+
+        Ok(results)
     }
 
-    fn part2(&self, _input: &str) -> Result<Box<dyn Display>> {
-        Ok(Box::new(0))
+    fn check(left: &JsonValue, right: &JsonValue) -> Ordering {
+        // Two integers
+        if let (Some(left), Some(right)) = (left.as_i64(), right.as_i64()) {
+            return left.cmp(&right);
+        }
+
+        // Two arrays
+        if let (Some(left), Some(right)) = (left.as_array(), right.as_array()) {
+            for i in 0..left.len().max(right.len()) {
+                if i == left.len() {
+                    return Ordering::Less;
+                }
+
+                if i == right.len() {
+                    return Ordering::Greater;
+                }
+
+                let result = Self::check(&left[i], &right[i]);
+                if result == Ordering::Equal {
+                    continue;
+                }
+
+                return result;
+            }
+
+            return Ordering::Equal;
+        }
+
+        if let Some(right) = right.as_i64() {
+            return Self::check(left, &serde_json::to_value(vec![right]).unwrap());
+        }
+
+        Self::check(
+            &serde_json::to_value(vec![left.as_i64().unwrap()]).unwrap(),
+            right,
+        )
+    }
+}
+
+impl Solution for DaySolution {
+    fn part1(&self, input: &str) -> Result<Box<dyn Display>> {
+        let items = self.parse(input)?;
+        let result = items
+            .iter()
+            .tuples::<(_, _)>()
+            .map(|(left, right)| Self::check(left, right))
+            .enumerate()
+            .filter_map(|(i, r)| {
+                if r == Ordering::Less {
+                    return Some(i + 1);
+                }
+
+                None
+            })
+            .sum::<usize>();
+
+        Ok(Box::new(result))
+    }
+
+    fn part2(&self, input: &str) -> Result<Box<dyn Display>> {
+        let mut items = self.parse(input)?;
+        items.push(serde_json::to_value(vec![vec![2]])?);
+        items.push(serde_json::to_value(vec![vec![6]])?);
+
+        items.sort_by(Self::check);
+
+        let result = items
+            .iter()
+            .enumerate()
+            .filter_map(|(i, v)| {
+                let s = serde_json::to_string(v).unwrap();
+                if s == "[[2]]" || s == "[[6]]" {
+                    return Some(i + 1);
+                }
+
+                None
+            })
+            .product::<usize>();
+
+        Ok(Box::new(result))
     }
 }
 
@@ -28,6 +117,6 @@ mod test {
     use aoc::day_test;
     use paste::paste;
 
-    day_test!(day08, Part1, "inputs/day08_demo.txt", "unknown");
-    day_test!(day08, Part2, "inputs/day08_demo.txt", "unknown");
+    day_test!(day13, Part1, "inputs/day13_demo.txt", "13");
+    day_test!(day13, Part2, "inputs/day13_demo.txt", "140");
 }
--- src/main.rs
+++ src/main.rs
@@ -14,24 +14,27 @@ mod day09;
 mod day10;
 mod day11;
 mod day12;
+mod day13;
 
 fn main() {
     let day = std::env::args()
         .nth(1)
         .and_then(|v| v.parse::<usize>().ok());
 
-    runners!(day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12)
-        .iter()
-        .enumerate()
-        .filter(|(d, _)| day.map(|day| day - 1 == *d).unwrap_or(true))
-        .for_each(|(day, runner)| {
-            println!("Day #{:02}\n{}", day + 1, "-".repeat(32));
+    runners!(
+        day01, day02, day03, day04, day05, day06, day07, day08, day09, day10, day11, day12, day13
+    )
+    .iter()
+    .enumerate()
+    .filter(|(d, _)| day.map(|day| day - 1 == *d).unwrap_or(true))
+    .for_each(|(day, runner)| {
+        println!("Day #{:02}\n{}", day + 1, "-".repeat(32));
 
-            aoc::SolutionPart::iter().for_each(|part| match runner.run(part) {
-                Ok(result) => println!("{}: {}", part, result),
-                Err(err) => println!("{}: {}", part, err),
-            });
-
-            println!();
+        aoc::SolutionPart::iter().for_each(|part| match runner.run(part) {
+            Ok(result) => println!("{}: {}", part, result),
+            Err(err) => println!("{}: {}", part, err),
         });
+
+        println!();
+    });
 }
