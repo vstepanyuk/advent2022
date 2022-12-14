0becb247e4a4f9a5d70449bc3f52167c0229e3f1
Vitali Stsepaniuk <contact@vitaliy.dev>
2022-12-14 20:25:13 +0100

    refactor(day14): iterators

--- src/day14.rs
+++ src/day14.rs
@@ -3,7 +3,7 @@ use std::{collections::HashMap, fmt::Display, str::FromStr};
 use anyhow::Result;
 use aoc::{Runnable, Solution};
 use aoc_derive::Runner;
-use itertools::process_results;
+use itertools::Itertools;
 
 #[derive(Runner)]
 #[aoc(file = "inputs/day14.txt")]
@@ -21,6 +21,21 @@ impl Point {
     fn new((x, y): (i32, i32)) -> Self {
         Self { x, y }
     }
+
+    fn fall_in<T>(&self, space: &HashMap<Self, T>) -> Option<Self> {
+        [(0, 1), (-1, 1), (1, 1)]
+            .iter()
+            .map(|(dx, dy)| Point::new((self.x + dx, self.y + dy)))
+            .find(|point| !space.contains_key(point))
+    }
+
+    fn between(start: &Point, end: &Point) -> impl Iterator<Item = Point> {
+        itertools::iproduct!(
+            start.x.min(end.x)..=start.x.max(end.x),
+            start.y.min(end.y)..=start.y.max(end.y)
+        )
+        .map(Point::new)
+    }
 }
 
 impl FromStr for Point {
@@ -29,7 +44,7 @@ impl FromStr for Point {
     fn from_str(s: &str) -> Result<Self> {
         let (x, y) = s
             .split_once(',')
-            .ok_or_else(|| anyhow::anyhow!("Invalid point"))?;
+            .ok_or_else(|| anyhow::anyhow!("Invalid point!"))?;
 
         Ok(Point {
             x: x.parse()?,
@@ -39,63 +54,26 @@ impl FromStr for Point {
 }
 
 impl DaySolution {
-    fn generate(&self, start: &Point, end: &Point) -> impl Iterator<Item = Point> {
-        let range_x = if start.x < end.x {
-            start.x..=end.x
-        } else {
-            end.x..=start.x
-        };
-
-        let range_y = if start.y < end.y {
-            start.y..=end.y
-        } else {
-            end.y..=start.y
-        };
-
-        itertools::iproduct!(range_x, range_y).map(Point::new)
-    }
-
-    fn parse(&self, input: &str) -> Result<HashMap<Point, char>> {
-        let mut cave = HashMap::new();
-
-        for line in input.lines() {
-            let points = process_results(line.split(" -> ").map(|s| s.parse::<Point>()), |iter| {
-                iter.collect::<Vec<_>>()
-            })?;
-
-            points.windows(2).for_each(|items| {
-                for point in self.generate(&items[0], &items[1]) {
-                    cave.insert(point, '#');
-                }
-            });
-        }
-
-        Ok(cave)
-    }
-
-    #[allow(dead_code)]
-    fn debug(&self, cave: &HashMap<Point, char>) {
-        let min_x = cave.keys().map(|p| p.x).min().unwrap();
-        let max_x = cave.keys().map(|p| p.x).max().unwrap();
-        let max_y = cave.keys().map(|p| p.y).max().unwrap();
-
-        for y in 0..=max_y {
-            for x in min_x..=max_x {
-                print!("{}", cave.get(&Point::new((x, y))).unwrap_or(&'.'));
-            }
-
-            println!();
-        }
-    }
-
-    fn can_move(&self, cave: &HashMap<Point, char>, point: &Point) -> bool {
-        !cave.contains_key(point)
+    fn parse(&self, input: &str) -> HashMap<Point, char> {
+        HashMap::from_iter(
+            input
+                .lines()
+                .map(|line| {
+                    line.split(" -> ")
+                        .map(|s| s.parse::<Point>().ok())
+                        .flatten()
+                        .tuple_windows()
+                        .map(|(start, end)| Point::between(&start, &end).map(|p| (p, '#')))
+                        .flatten()
+                })
+                .flatten(),
+        )
     }
 }
 
 impl Solution for DaySolution {
     fn part1(&self, input: &str) -> Result<Box<dyn Display>> {
-        let mut cave = self.parse(input)?;
+        let mut cave = self.parse(input);
         let max_height = cave.keys().map(|p| p.y).max().unwrap();
         let mut sand = Point::new((500, 0));
 
@@ -104,14 +82,8 @@ impl Solution for DaySolution {
                 break;
             }
 
-            if self.can_move(&cave, &Point::new((sand.x, sand.y + 1))) {
-                sand.y += 1;
-            } else if self.can_move(&cave, &Point::new((sand.x - 1, sand.y + 1))) {
-                sand.y += 1;
-                sand.x -= 1
-            } else if self.can_move(&cave, &Point::new((sand.x + 1, sand.y + 1))) {
-                sand.y += 1;
-                sand.x += 1
+            if let Some(new_pos) = sand.fall_in(&cave) {
+                sand = new_pos;
             } else {
                 cave.insert(sand, 'o');
                 sand = Point::new((500, 0));
@@ -122,7 +94,7 @@ impl Solution for DaySolution {
     }
 
     fn part2(&self, input: &str) -> Result<Box<dyn Display>> {
-        let mut cave = self.parse(input)?;
+        let mut cave = self.parse(input);
         let max_height = cave.keys().map(|p| p.y).max().unwrap();
         let mut sand = Point::new((500, 0));
 
@@ -133,14 +105,8 @@ impl Solution for DaySolution {
         );
 
         loop {
-            if self.can_move(&cave, &Point::new((sand.x, sand.y + 1))) {
-                sand.y += 1;
-            } else if self.can_move(&cave, &Point::new((sand.x - 1, sand.y + 1))) {
-                sand.y += 1;
-                sand.x -= 1
-            } else if self.can_move(&cave, &Point::new((sand.x + 1, sand.y + 1))) {
-                sand.y += 1;
-                sand.x += 1
+            if let Some(new_pos) = sand.fall_in(&cave) {
+                sand = new_pos;
             } else {
                 cave.insert(sand, 'o');
                 if sand.x == 500 && sand.y == 0 {
