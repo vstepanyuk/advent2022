df85b1b47e4457faaffca21bd533141c56264f1d
Vitali Stsepaniuk <contact@vitaliy.dev>
2022-12-18 23:43:31 +0100

    feat(day16, day17): partial

--- src/day16.rs
+++ src/day16.rs
@@ -1,24 +1,94 @@
-use std::fmt::Display;
+use std::{collections::HashMap, fmt::Display};
 
 use anyhow::Result;
 use aoc::{Runnable, Solution};
 use aoc_derive::Runner;
+use nom::{
+    self, branch::alt, bytes::complete::tag, bytes::complete::take, character::complete::digit1,
+    combinator::map_res, multi::separated_list1, sequence::tuple, IResult,
+};
+use pathfinding::prelude::dijkstra;
 
 #[derive(Runner)]
-#[aoc(file = "inputs/day08.txt")]
+#[aoc(file = "inputs/day16.txt")]
 pub struct DaySolution {
     pub filename: &'static str,
 }
 
-impl DaySolution {}
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Valve<'a> {
+    id: &'a str,
+    rate: i32,
+    leads_to: Vec<&'a str>,
+}
+
+impl Valve<'_> {
+    fn parse(i: &str) -> IResult<&str, Valve> {
+        let (_, (_, id, _, rate, _, leads_to)) = tuple((
+            tag("Valve "),
+            take(2usize),
+            tag(" has flow rate="),
+            map_res(digit1, str::parse),
+            alt((
+                tag("; tunnels lead to valves "),
+                tag("; tunnel leads to valve "),
+            )),
+            separated_list1(tag(", "), take(2usize)),
+        ))(i)?;
+
+        Ok((i, Valve { id, rate, leads_to }))
+    }
+}
+
+impl DaySolution {
+    fn parse<'a>(&self, input: &'a str) -> HashMap<&'a str, Valve<'a>> {
+        HashMap::from_iter(input.lines().map(|line| {
+            let (_, valve) = Valve::parse(line).unwrap();
+
+            (valve.id, valve)
+        }))
+    }
+}
 
 impl Solution for DaySolution {
-    fn part1(&self, _input: &str) -> Result<Box<dyn Display>> {
-        Ok(Box::new(0))
+    fn part1(&self, input: &str) -> Result<Box<dyn Display>> {
+        let valves = self.parse(input);
+
+        let max_rate = valves
+            .values()
+            .map(|valve| valve.rate)
+            .max()
+            .unwrap_or_default();
+
+        dbg!(max_rate);
+
+        let result = dijkstra(
+            &("AA".to_owned(), 30),
+            |(current, minutes)| {
+                dbg!(current, minutes);
+
+                let mut result = vec![];
+
+                for lead in valves.get(current.as_str()).unwrap().leads_to.iter() {
+                    let valve = valves.get(lead).unwrap();
+                    let rate = valve.rate;
+
+                    result.push(((lead.to_string(), *minutes - 2), max_rate - rate));
+                }
+
+                result
+            },
+            |&(_, minutes)| minutes == 0,
+        );
+
+        println!("{:?}", result);
+        println!("{:?}", valves);
+
+        Err(anyhow::anyhow!("Not implemented"))
     }
 
     fn part2(&self, _input: &str) -> Result<Box<dyn Display>> {
-        Ok(Box::new(0))
+        Err(anyhow::anyhow!("Not implemented"))
     }
 }
 
@@ -28,6 +98,6 @@ mod test {
     use aoc::day_test;
     use paste::paste;
 
-    day_test!(day08, Part1, "inputs/day08_demo.txt", "unknown");
-    day_test!(day08, Part2, "inputs/day08_demo.txt", "unknown");
+    day_test!(day16, Part1, "inputs/day16_demo.txt", "unknown");
+    day_test!(day16, Part2, "inputs/day16_demo.txt", "unknown");
 }
--- src/day17.rs
+++ src/day17.rs
@@ -1,20 +1,202 @@
-use std::fmt::Display;
+use std::{collections::HashSet, fmt::Display};
 
 use anyhow::Result;
 use aoc::{Runnable, Solution};
 use aoc_derive::Runner;
 
 #[derive(Runner)]
-#[aoc(file = "inputs/day08.txt")]
+#[aoc(file = "inputs/day17.txt")]
 pub struct DaySolution {
     pub filename: &'static str,
 }
 
-impl DaySolution {}
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Point {
+    x: usize,
+    y: usize,
+}
+
+#[derive(Debug, Clone)]
+struct Rock {
+    points: Vec<Point>,
+}
+
+impl Rock {
+    fn new(shape: &str) -> Self {
+        let mut points = vec![];
+        for (y, line) in shape.lines().rev().enumerate() {
+            for (x, c) in line.chars().enumerate() {
+                if c == '#' {
+                    points.push(Point { x, y });
+                }
+            }
+        }
+
+        Rock { points }
+    }
+
+    fn all_available() -> Vec<Rock> {
+        vec![
+            Self::new("####"),
+            Self::new(".#.\n###\n.#."),
+            Self::new("..#\n..#\n###"),
+            Self::new("#\n#\n#\n#"),
+            Self::new("##\n##"),
+        ]
+    }
+
+    fn move_left(&mut self, map: &HashSet<Point>) -> bool {
+        if self
+            .points
+            .iter()
+            .any(|p| p.x == 0 || map.contains(&Point { x: p.x - 1, y: p.y }))
+        {
+            return false;
+        }
+        self.points.iter_mut().for_each(|p| p.x -= 1);
+
+        true
+    }
+
+    fn move_right(&mut self, map: &HashSet<Point>) -> bool {
+        if self
+            .points
+            .iter()
+            .any(|p| p.x == 6 || map.contains(&Point { x: p.x + 1, y: p.y }))
+        {
+            return false;
+        }
+        self.points.iter_mut().for_each(|p| p.x += 1);
+
+        true
+    }
+
+    fn top(&self) -> usize {
+        self.points.iter().map(|p| p.y).min().unwrap()
+    }
+
+    fn bottom(&self) -> usize {
+        self.points.iter().map(|p| p.y).max().unwrap()
+    }
+
+    fn height(&self) -> usize {
+        self.bottom() - self.top() + 1
+    }
+
+    fn move_by(&mut self, x: usize, y: usize) {
+        self.points.iter_mut().for_each(|p| {
+            p.x += x;
+            p.y += y;
+        });
+    }
+
+    // fn moved_by(directions: &[char]) -> Self {
+    //     // let rock.move_by(2, 0);
+    //     // for c in directions.chars() {
+    //     //     match c {
+    //     //         'L' => self.move_left(&HashSet::new()),
+    //     //         'R' => self.move_right(&HashSet::new()),
+    //     //         'D' => self.down(&HashSet::new()),
+    //     //         _ => {}
+    //     //     }
+    //     // }
+    // }
+
+    fn down(&mut self, map: &HashSet<Point>) -> bool {
+        if self
+            .points
+            .iter()
+            .any(|p| p.y == 0 || map.contains(&Point { x: p.x, y: p.y - 1 }))
+        {
+            return false;
+        }
+
+        self.points.iter_mut().for_each(|p| p.y -= 1);
+        true
+    }
+}
+
+impl DaySolution {
+    fn debug(&self, map: &HashSet<Point>, max_y: usize, current: Option<&Rock>) {
+        for y in (0..=max_y).rev() {
+            print!("|");
+            for x in 0..=6 {
+                if map.contains(&Point { x, y }) {
+                    print!("#");
+                } else {
+                    if let Some(rock) = current {
+                        if rock.points.contains(&Point { x, y }) {
+                            print!("@");
+                            continue;
+                        }
+                    }
+                    print!(".");
+                }
+            }
+            print!("|");
+            println!();
+        }
+
+        println!("+-------+");
+    }
+}
 
 impl Solution for DaySolution {
-    fn part1(&self, _input: &str) -> Result<Box<dyn Display>> {
-        Ok(Box::new(0))
+    fn part1(&self, input: &str) -> Result<Box<dyn Display>> {
+        let rocks = Rock::all_available();
+        let mut map = HashSet::<Point>::new();
+        let mut y = 3;
+        let gas_diretions = input.chars().collect::<Vec<_>>();
+        let mut rock_index = 0;
+        let mut direction_index = 0;
+
+        // let now = std::time::Instant::now();
+
+        for _ in 0..5000 {
+            let mut rock = rocks[rock_index % rocks.len()].clone();
+            rock.move_by(2, y);
+            // println!("Rock: #{i}");
+            // self.debug(&map, y + rock.height() - 1, Some(&rock));
+
+            // if i % 1_000_000i64 == 0 {
+            //     println!("i: {}", i);
+
+            //     let elapsed = now.elapsed();
+            //     println!("Elapsed: {:?}", elapsed);
+            // }
+
+            loop {
+                let direction = gas_diretions[direction_index % gas_diretions.len()];
+                if direction == '>' {
+                    _ = !rock.move_right(&map);
+                } else {
+                    _ = !rock.move_left(&map);
+                }
+
+                // self.debug(&map, y + rock.height() - 1, Some(&rock));
+
+                direction_index += 1;
+                if !rock.down(&map) {
+                    break;
+                }
+            }
+
+            map.extend(rock.points.iter().cloned());
+
+            // println!();
+            // println!();
+
+            rock_index += 1;
+            y = y.max(rock.top() + rock.height() + 3);
+        }
+
+        // dbg!()
+        self.debug(&map, y, None);
+        let max_y = map.iter().map(|p| p.y).max().unwrap() + 1;
+
+        dbg!(max_y);
+
+        Ok(Box::new(max_y))
     }
 
     fn part2(&self, _input: &str) -> Result<Box<dyn Display>> {
@@ -28,6 +210,6 @@ mod test {
     use aoc::day_test;
     use paste::paste;
 
-    day_test!(day08, Part1, "inputs/day08_demo.txt", "unknown");
-    day_test!(day08, Part2, "inputs/day08_demo.txt", "unknown");
+    day_test!(day17, Part1, "inputs/day17_demo.txt", "3068");
+    day_test!(day17, Part2, "inputs/day17_demo.txt", "unknown");
 }
